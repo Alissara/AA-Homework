4e425d11fe38f3f1c581229c9f7a938355eb50b3
alissara <alissara.r@gmail.com>
2017-04-14 13:50:59 -0700

    Add w3

--- w2d2/skeleton/lib/board.rb
+++ w2d2/skeleton/lib/board.rb
@@ -4,23 +4,21 @@ class Board
   def initialize(name1, name2)
     @name1 = name1
     @name2 = name2
-    @cups = Array.new(14){[]}
+    @cups = Array.new(14) {[]}
     place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
-    @cups.each_with_index do |cup, idx|
+    @cups.each.with_index do |cup, idx|
       unless idx == 6 || idx == 13
-        4.times do
-          cup << :stone
-        end
+        cup.concat([:stone, :stone, :stone, :stone])
       end
     end
   end
 
   def valid_move?(start_pos)
-    raise "Invalid starting cup" if start_pos < 1 || start_pos > 12
+    raise 'Invalid starting cup' unless start_pos.between?(1, 12)
   end
 
   def make_move(start_pos, current_player_name)
@@ -32,16 +30,37 @@ class Board
       cup_idx += 1
       cup_idx = 0 if cup_idx == 14
 
+      next if cup_idx == 6 && current_player_name == @name2
+      next if cup_idx == 13 && current_player_name == @name1
       @cups[cup_idx] << stones.pop
+
     end
 
-
-
+    render
+    next_turn(cup_idx)
   end
 
   def next_turn(ending_cup_idx)
-    # helper method to determine what #make_move returns
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      :switch
+    else
+      ending_cup_idx
+    end
+  end
 
+  def one_side_empty?
+    @cups[0..5].all?(&:empty?) ||
+    @cups[7..12].all?(&:empty?)
+  end
+
+  def winner
+    if @cups[6].count == @cups[13].count
+      :draw
+    else
+      @cups[6].count > @cups[13].count ? @name1 : @name2
+    end
   end
 
   def render
@@ -52,16 +71,4 @@ class Board
     puts ""
   end
 
-  def one_side_empty?
-    @cups[1..5].all? {|cup| cup.empty?} ||
-    @cups[7..12].all? {|cup| cup.empty?}
-  end
-
-  def winner
-    if @cups[6].count == @cups[13].count
-      :draw
-    else
-      @cups[6].count > @cups[13].count ? @name1 : @name2
-    end
-  end
 end
